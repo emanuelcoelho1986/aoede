7f7269665c2eeb6e48da33b47165982fd6aa6c46
Emanuel Coelho <emanuelcoelho1986@gmail.com>
2022-01-17 18:56:03 +0000

    clear[skeleton] from example generated with ng init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'aoede';
+export class AppComponent implements OnInit {
+  title = 'Aeode - Muse of voice and Song';
+
+  constructor(private titleService: Title) { }
+
+
+  ngOnInit() {
+    this.titleService.setTitle(this.title);
+  }
+
 }
