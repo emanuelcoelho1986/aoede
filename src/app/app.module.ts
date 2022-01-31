04f35325d8c66c86e9ae3bbc210b66840b980066
Emanuel Coelho <emanuelcoelho1986@users.noreply.github.com>
2022-01-31 19:36:19 +0000

    As a user, I don't like the material cards
    removed[angular-material] (#17)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,7 @@ import {AppRoutingModule} from './routes/app-routing.module';
 import {AppComponent} from './app.component';
 import {BlogModule} from './modules/blog/blog.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatCardModule} from "@angular/material/card";
 import {BlogCardComponent} from "./components/blog-card/blog-card.component";
-import {MatButtonModule} from "@angular/material/button";
-import {MatDividerModule} from "@angular/material/divider";
-import {MatProgressBarModule} from "@angular/material/progress-bar";
 import {SafePipe} from './pipes/safe.pipe';
 import {CardCommentsComponent} from './components/card-comments/card-comments.component';
 
@@ -29,12 +25,7 @@ import {CardCommentsComponent} from './components/card-comments/card-comments.co
 
     BlogModule,
 
-    BrowserAnimationsModule,
-
-    MatProgressBarModule,
-    MatDividerModule,
-    MatButtonModule,
-    MatCardModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
