5ad08186808cd1a35c4c11fc1aa18c4cb4978d5d
Emanuel Coelho <emanuelcoelho1986@users.noreply.github.com>
2022-04-06 21:58:02 +0100

    fix[navigation] issue caused by seo tags manipulation (#36)

--- src/app/routes/app-routing.module.ts
+++ src/app/routes/app-routing.module.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
     ]
     */
   },
-  // Feels like reddit this way
+  // Feels more like reddit this way
   {
     path: 'p/:slug/:id',
     component: BlogPostComponent,
